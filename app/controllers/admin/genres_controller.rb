f20122d0c14afdd6d3f7536a5a39a9f6bb255aa5
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-09-19 12:33:53 +0000

    ジャンル編集機能の完成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -20,6 +20,12 @@ class Admin::GenresController < ApplicationController
     end
     
     def update
+        @genre = Genre.find(params[:id])
+        if @genre.update(genre_params)
+            redirect_to admin_genres_path
+        else
+            render :edit
+        end
     end
     
     private
