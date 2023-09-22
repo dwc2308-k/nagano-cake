e975c0be6f1580af0bda1c0b003c5ccc01352b8d
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-09-22 17:00:18 +0000

    顧客の会員登録作成、バリデーション記述

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,16 +3,16 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-         
-         
-         
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :postcode, presence: true
-         validates :address, presence: true
-         validates :phone_number, presence: true
-         validates :email, presence: true
+
+
+        validates :encrypted_password_confirmation, presence: true
+        validates :encrypted_password, length: { minimum: 1, maximum: 6 }
+        validates :last_name, presence: true
+        validates :first_name, presence: true
+        validates :last_name_kana, presence: true
+        validates :first_name_kana, presence: true
+        validates :postcode, presence: true, format: { with: /\A\d{10,11}\z/ }
+        validates :address, presence: true
+        validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+        validates :email, presence: true
 end
