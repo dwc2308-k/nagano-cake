d59d4a5ec35d4a26c8fcbcbd93a89de9a0ac19a9
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-09-22 09:38:02 +0000

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
   # 顧客用
 # URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
+  devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+ 
   namespace :public do
-     resources :customers, only: [:show, :edit, :update,:unsubscribe]
-     resources :homes, only: [:top,:about]
+    resources :customers, only: [:show, :edit, :update,:unsubscribe]
+    resources :homes, only: [:top,:about]
     resources :items, only: [:index,:show]
   end
 
-  devise_for :customers
-  devise_for :users
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
