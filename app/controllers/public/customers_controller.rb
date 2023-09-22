87645a4bca6514c914a68a1b1c3a219b0030d640
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-09-22 03:54:00 +0000

    退会機能追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,22 @@
 class Public::CustomersController < ApplicationController
-  def edit
-  end
-
+  
   def show
     @customer = current_costomer
   end
-
+  
   def unsubscribe
   end
+  
+  def withdraw
+  end
+  
+  
+  def edit
+    @customer = current_costomer
+  end
+
+  
+  def update
+  end
+  
 end
