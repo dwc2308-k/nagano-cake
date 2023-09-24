5d05baa26742bea7bcdeb56be1d5cd9ccf6cec05
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-09-24 12:40:12 +0000

    cart_itemモデル、コントローラーの作成、ビューも途中まで完成

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,9 +1,6 @@
 class Admin::HomesController < ApplicationController
-    
-    has_many :orders
-    has_many :order_details
-    
     def top
         @orders = Order.all
+        
     end
 end
---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,44 @@
+class Public::CartItemsController < ApplicationController
+    
+    def index
+        @cart_items = CartItem.all
+    end
+    
+    def update
+        cart_item = CartItem.find(params[:id])
+        cart_item.update(cart_item_params)
+        redirect_to request.referer
+    end
+    
+    def create
+        cart_item = CartItem.new(cart_item_params)
+        cart_item.customer_id = current_customer.id
+        cart_item.item_id = cart_item_params[:item_id]
+        if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+            cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+            cart_item.amount += params[:cart_item][:amount].to_i
+            cart_item.update(amount: cart_item.amount)
+            redirect_to request.referer
+        else
+            cart_item.save
+            redirect_to request.referer
+        end
+    end
+    
+    def destroy
+        cart_item = CartItem.find(params[:id])
+        cart_item.destroy
+        redirect_to request.referer
+    end
+    
+    def destroy_all
+        current_user.cart_items.destroy_all
+        redirect_to request.referer
+    end
+    
+    private
+    def cart_item_params
+        params.require(:cart_item).permit(:item_id, :amount)
+    end
+    
+end
