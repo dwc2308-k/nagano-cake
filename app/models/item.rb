48f95a991b8b03be477d6a88a24afb9378147f50
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-09-25 06:49:50 +0000

    追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,12 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_one_attached :image
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :genre_id, presence: true
+  validates :price_excluding_tax, presence: true, numericality: { only_integer: true, greater_than: 0}
+
+
+
 end
 
