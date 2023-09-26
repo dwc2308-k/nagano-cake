c5b9e4e4bb03fa8f835da92038dc4d41ea847fba
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-09-26 10:04:21 +0000

    顧客情報編集機能作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -18,8 +18,13 @@ class Public::CustomersController < ApplicationController
 
   def update
      @customer = current_customer
-     @customer.update
-     redirect_to public_customers_mypage_path(@customer)
+     @customer.update(customer_params)
+     redirect_to public_customers_mypage_path
   end
+  
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
+    end
 
 end
