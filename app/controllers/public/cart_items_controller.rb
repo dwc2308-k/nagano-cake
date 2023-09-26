38aa2885fd0590440d948759f85a634f3feee89b
bokoriai <139733346+bokoriai@users.noreply.github.com>
2023-09-26 07:27:16 +0000

    カート内一覧画面完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,8 @@
 class Public::CartItemsController < ApplicationController
 
     def index
-        @cart_items = CartItem.where(customer_id: current_customer.id)
-        @total = @cart_items.inject(0) { |sum, item| sum + cart_item.subtotal }
+        @cart_items = current_customer.cart_items.all
+        @total = 0
     end
 
     def update
@@ -12,17 +12,18 @@ class Public::CartItemsController < ApplicationController
     end
 
     def create
-        cart_item = CartItem.new(cart_item_params)
-        cart_item.customer_id = current_customer.id
-        cart_item.item_id = cart_item_params[:item_id]
-        if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
-            cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+        @cart_item = current_customer.cart_items.new(cart_item_params)
+        if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+            cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
             cart_item.amount += params[:cart_item][:amount].to_i
-            cart_item.update(amount: cart_item.amount)
-            redirect_to request.referer
-        else
             cart_item.save
-            redirect_to request.referer
+            redirect_to public_cart_items_path
+        elsif
+            @cart_item.save
+            @cart_items = current_customer.cart_items.all
+            redirect_to public_cart_items_path
+        else
+            render 'index'
         end
     end
     
